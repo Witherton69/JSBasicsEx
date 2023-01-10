7e779c0bb5a4f6f098eae69cbaac1f1527ee7cf8
Witherton <katelyn.gosnell@stu.barren.kyschools.us>
2023-01-10 10:55:34 -0600

    Adding java stuff and trying to remember how to add stuff to git

---
+++ script.js
@@ -0,0 +1,50 @@
+// Exercise 
+
+// 1
+console.log(25);
+
+// 2
+// Style, Numbers, Strings, Null, Boolean, Undifined
+
+// 3
+// +, -, **, *, /, %
+
+// 4
+console.log(1 % 5); //1 
+console.log(6 % 5); //1
+
+// 5
+console.log('I\'M MARY POPPINS, Y\'ALL'); // I'M MARY POPPINS, Y'ALL
+
+// 6
+console.log("Hawk" + "Eye"); //HawkEye
+
+// 7
+console.log("Kyle".length + " Witherton".length); //14
+
+// 8
+console.log("Avenger: Endgame"[15]); //e
+
+// BONUS (I don't think I did half of these correctly.)
+
+// 1
+console.log('\\'); // \
+
+// 2
+console.log(5 ** 4 - 9); // 616
+
+// 3 
+console.log("I" + "Am" + "Iron" + "Man" + "".length); //  IAmIronMan0
+
+// 4
+console.log("CAP"[1]); // A
+console.log("VISION"[0]); // V
+console.log("OKOYE"[4]); // E
+console.log("NICK FURY"[0]); // N
+console.log("GROOT"[0]); // G
+console.log("DOCTOR STRANGE"[13]); // E
+console.log("THOR"[3]); // R
+console.log("STAR-LORD"[0]); // S
+
+// 5
+// (I had no clue what you were asking.)
